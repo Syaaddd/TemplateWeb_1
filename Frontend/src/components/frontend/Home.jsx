aff72b8448561e33f40ef2163af222d51feee9fe
Arsyad2009 <105086466+Arsyad2009@users.noreply.github.com>
2024-12-01 09:20:58 +0700

    update

--- Frontend/src/components/frontend/Home.jsx
+++ Frontend/src/components/frontend/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import AboutImg from '../../assets/images/about-us.jpg';
 
 const Home = () => {
   return (
@@ -10,8 +11,8 @@ const Home = () => {
         <div className='container py-3'>
           <Navbar expand="lg">
                 <Navbar.Brand href="/" className='logo'>
-                  <span>UrbanEdge </span>
-                Constructions
+                  <span>Name </span>
+                 Website
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
@@ -28,22 +29,66 @@ const Home = () => {
         </div>
       </header>
       <main>
+         {/*Hero Section */}
         <section className='section-1'>
             <div className='hero d-flex align-items-center'>
               <div className='container-fluid'>
                 <div className='text-center'>
-                  <span>Welcome Amazing Constructions</span>
+                  <span>Welcome Lorem, ipsum dolor.</span>
                   <h1>Lorem ipsum dolor sit amet. <br />Lorem, ipsum.</h1>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, sint!</p>
-                  <a className='btn btn-primary'>Contact Now</a>
-                  <a className='btn btn-secondary ms-2'>View Projects</a>
+                  <div className='mt-4'>
+                    <a className='btn btn-primary'>Contact Now</a>
+                    <a className='btn btn-secondary ms-2'>View Projects</a>
+                  </div>
                 </div>
               </div>
             </div>
         </section>
+
+        {/*About Us Section */}
+        <section className='section-2 py-5'>
+            <div className='container py-5'>
+                <div className='row'>
+                  <div className='col-md-6'>
+                    <img src={AboutImg} c className='w-100' />
+                  </div>
+
+                  <div className='col-md-6'>
+                    <span>about us</span>
+                    <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                      Minus sint consequatur optio voluptas, fuga ex quam necessitatibus 
+                      beatae nostrum adipisci mollitia asperiores dignissimos repudiandae
+                    </p>
+
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                      Minus sint consequatur optio voluptas, fuga ex quam necessitatibus 
+                      beatae nostrum adipisci mollitia asperiores dignissimos repudiandae
+                       vitae laborum quae, excepturi possimus! Nobis aliquam officiis, velit s.
+                    </p>
+                  </div>
+                </div>
+            </div>
+        </section>
       </main>
       <footer>
+          <div className='container'>
+              <div className='col-md-3'>
 
+              </div>
+              <div className='col-md-3'>
+
+              </div>
+              <div className='col-md-3'>
+
+              </div>
+              <div className='col-md-3'>
+
+              </div>
+          </div>
       </footer>
     </>
   )
